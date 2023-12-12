3b0c3a0014877d3b2e44eb190b53ee4dbca307ab
Sandip Pathak <pathaksandip321@gmail.com>
2023-12-12 08:01:00 +0545

    GradeSheet

--- srs/src/Components/Pages/Result.jsx
+++ srs/src/Components/Pages/Result.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import ReactToPdf from "react-to-pdf";
-
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+import html2canvas from "html2canvas";
 function Result() {
   const [examNames, setExamNames] = useState([]);
   const [selectedExamName, setSelectedExamName] = useState("");
@@ -9,8 +10,10 @@ function Result() {
   const [selectedClassName, setselectedClassName] = useState("");
   const [obtainedMarksData, setObtainedMarksData] = useState([]);
   const [subjectNames, setSubjectNames] = useState([]);
+  const [showGradePoints, setShowGradePoints] = useState(true); // New state
   const ref = React.createRef();
   const [pdfLoading, setPdfLoading] = useState(false);
+
   useEffect(() => {
     const fetchExamNames = async () => {
       try {
@@ -51,7 +54,8 @@ function Result() {
           return examResult.students.map((student) => {
             const totalMarks = student.subjects.reduce((total, subject) => {
               const obtainedMarks = subject.obtainedMarks;
-              const subjectMarks = obtainedMarks.match(/.{2}/g);
+              const subjectMarks = obtainedMarks.match(/(\d+)/g);
+
               return (
                 total +
                 (subjectMarks
@@ -63,9 +67,27 @@ function Result() {
               );
             }, 0);
 
+            const gradePointsArray = student.subjects.map((subject) => {
+              return calculateSubjectGradePoints(
+                subject.obtainedMarks,
+                subject.fullMarks
+              );
+            });
+
+            const totalGradePoints =
+              gradePointsArray.length > 0
+                ? (
+                    gradePointsArray.reduce(
+                      (sum, gradePoint) => sum + gradePoint,
+                      0
+                    ) / gradePointsArray.length
+                  ).toFixed(2)
+                : 0;
+
             return {
               ...student,
               totalMarks,
+              totalGradePoints,
             };
           });
         });
@@ -90,6 +112,225 @@ function Result() {
     fetchExamNames();
     fetchObtainedMarksData();
   }, [selectedExamName, selectedClassName]);
+
+  const calculateSubjectGradePoints = (obtainedMarks, fullMarks) => {
+    const percentage = (obtainedMarks / fullMarks) * 100;
+
+    if (percentage >= 90) {
+      return 4.0;
+    } else if (percentage >= 80) {
+      return 3.6;
+    } else if (percentage >= 70) {
+      return 3.2;
+    } else if (percentage >= 60) {
+      return 2.8;
+    } else if (percentage >= 50) {
+      return 2.4;
+    } else if (percentage >= 40) {
+      return 2.0;
+    } else if (percentage >= 35) {
+      return 1.6;
+    } else {
+      return 0.0; // Default grade point for marks below 40% of full marks
+    }
+  };
+
+  const handleGenerateGradeSheet = async () => {
+    try {
+      setPdfLoading(true);
+
+      // Create a new jsPDF instance
+      const doc = new jsPDF();
+
+      // Iterate over each student and add their details to the PDF
+      for (let index = 0; index < obtainedMarksData.length; index++) {
+        const student = obtainedMarksData[index];
+
+        // Add page for each student
+        if (index > 0) {
+          doc.addPage();
+        }
+
+        // Set font size and add header
+        const headingFontSize = 40; // Set your desired font size
+        const headingFontStyle = "bold"; // Set the font style to bold
+        const headingMargin = 7;
+        const borderMargin = 5; // Set your desired margin inside the border
+
+        // Draw a border around the page with a 5-pixel margin inside
+        const pageWidth = doc.internal.pageSize.width;
+        const pageHeight = doc.internal.pageSize.height;
+        const borderWidth = 2; // Set your desired border width
+
+        doc.setLineWidth(borderWidth);
+        doc.rect(
+          borderMargin,
+          borderMargin,
+          pageWidth - 2 * borderMargin,
+          pageHeight - 2 * borderMargin
+        );
+
+        const whitishDarkColor = [100, 100, 100];
+        doc.setDrawColor(
+          whitishDarkColor[0],
+          whitishDarkColor[1],
+          whitishDarkColor[2]
+        );
+        doc.setTextColor(
+          whitishDarkColor[0],
+          whitishDarkColor[1],
+          whitishDarkColor[2]
+        );
+
+        doc.setFontSize(headingFontSize);
+        doc.setFont(headingFontStyle);
+        // Center the heading "School Result System"
+        const textWidth =
+          (doc.getStringUnitWidth("School Result System") * headingFontSize) /
+          doc.internal.scaleFactor;
+        const xOffset = (doc.internal.pageSize.width - textWidth) / 2;
+
+        doc.text("School Result System", xOffset, 10 + headingMargin);
+
+        // Set font size and style for the rest of the document
+        const selectedExamFontSize = 20;
+        const selectedExamFontStyle = "bold";
+        doc.setFontSize(selectedExamFontSize);
+        doc.setFont(selectedExamFontStyle);
+        doc.text(` ${selectedExamName}`, 70, 27);
+
+        doc.setFontSize(16);
+        doc.setFont("normal");
+
+        // Set the fill color for the "PROGRESS REPORT" row
+        const progressReportBackgroundColor = [0, 0, 0]; // Black color
+        doc.setFillColor(
+          progressReportBackgroundColor[0],
+          progressReportBackgroundColor[1],
+          progressReportBackgroundColor[2]
+        );
+
+        const progressReportWidth = 200; // Set your desired width
+        doc.rect(5, 31, progressReportWidth, 10, "F");
+
+        // Set text color for "PROGRESS REPORT"
+        const progressReportTextColor = [255, 255, 255]; // White color
+        doc.setTextColor(
+          progressReportTextColor[0],
+          progressReportTextColor[1],
+          progressReportTextColor[2]
+        );
+
+        // Add "PROGRESS REPORT" text
+        doc.text(`PROGRESS REPORT`, 80, 37);
+        doc.setFontSize(16);
+        doc.setFont("normal");
+        doc.setTextColor(0, 0, 0);
+        //issueddate
+        const today = new Date();
+        const formattedDate = `${today.getFullYear()}-${(today.getMonth() + 1)
+          .toString()
+          .padStart(2, "0")}-${today.getDate().toString().padStart(2, "0")}`;
+
+        // Add "Issued Date"
+        doc.text(`Issued Date: ${formattedDate}`, 140, 46);
+
+        doc.text(`Student Name: ${student.name}`, 10, 70);
+        doc.text(`Roll Number: ${student.rollNumber}`, 10, 80);
+
+        // Add table with subjects, obtained marks, etc.
+        doc.autoTable({
+          startY: 90,
+          head: [["S.N", "Subject", "Grade Points", "GPA"]],
+          body: student.subjects.map((subject, sn) => {
+            const gradePoints = showGradePoints
+              ? calculateSubjectGradePoints(
+                  subject.obtainedMarks,
+                  subject.fullMarks
+                ).toFixed(2)
+              : "";
+
+            const gpa = calculateGPA(gradePoints); // Add a function to calculate GPA
+
+            return [sn + 1, subject.subject, gradePoints, gpa];
+          }),
+          styles: {
+            cellPadding: 2,
+            fontSize: 12,
+            lineColor: [0, 0, 0], // Border color (black)
+            lineWidth: 0.5, // Border width
+            textColor: [0, 0, 0], // Text color (black)
+          },
+          headStyles: {
+            fillColor: [200, 200, 200], // Header background color
+            textColor: [0, 0, 0], // Header text color
+          },
+        });
+
+        // Add total marks, total grade points, rank, remarks, etc.
+        doc.text(
+          `Total Marks: ${student.totalMarks}`,
+          10,
+          doc.autoTable.previous.finalY + 10
+        );
+        doc.text(
+          `Total Grade Points: ${student.totalGradePoints}`,
+          10,
+          doc.autoTable.previous.finalY + 20
+        );
+        doc.text(
+          `Rank: ${student.rank}`,
+          10,
+          doc.autoTable.previous.finalY + 30
+        );
+        doc.text(
+          `Remarks: ${
+            student.subjects.every(
+              (subject) =>
+                parseInt(subject.obtainedMarks, 10) >= subject.passMarks
+            )
+              ? "Passed"
+              : "Failed"
+          }`,
+          10,
+          doc.autoTable.previous.finalY + 40
+        );
+      }
+
+      // Get the blob URL of the PDF
+      const blob = doc.output("blob");
+      const blobUrl = URL.createObjectURL(blob);
+
+      // Show the preview in a new window
+      const previewWindow = window.open(blobUrl, "_blank");
+
+      // Optionally, you can set a timeout and close the preview window after a few seconds
+      setPdfLoading(false);
+    } catch (error) {
+      console.error("Error generating grade sheet:", error);
+      setPdfLoading(false);
+    }
+  };
+
+  function calculateGPA(gradePoints) {
+    if (gradePoints >= 4.0) {
+      return "A+";
+    } else if (gradePoints >= 3.6) {
+      return "A";
+    } else if (gradePoints >= 3.2) {
+      return "B+";
+    } else if (gradePoints >= 2.8) {
+      return "B";
+    } else if (gradePoints >= 2.4) {
+      return "C+";
+    } else if (gradePoints >= 2.0) {
+      return "C";
+    } else if (gradePoints >= 1.6) {
+      return "D+";
+    } else {
+      return "NG";
+    }
+  }
 
   const handlePrint = () => {
     try {
@@ -139,6 +380,10 @@ function Result() {
     setselectedClassName(event.target.value);
   };
 
+  const handleToggleGradePoints = () => {
+    setShowGradePoints((prev) => !prev);
+  };
+
   const tableStyles = {
     display: "none",
   };
@@ -185,12 +430,26 @@ function Result() {
         <br />
 
         <div className="row">
-          <div className="col-md-6">
+          <div className="col-md-3">
             <h5> class: {selectedClassName}</h5>
           </div>
-          <div className="col-md-6">
+
+          <div className="col-md-4">
             <h5>Exam:{selectedExamName}</h5>
           </div>
+          <button
+            className="col-md-2 mb-2 btn btn-primary"
+            onClick={handleToggleGradePoints}
+          >
+            {showGradePoints ? "Hide Grade Points" : "Show Grade Points"}
+          </button>
+          <div className="col-md-1"></div>
+          <button
+            className="col-md-2 mb-2 btn btn-primary "
+            onClick={handleGenerateGradeSheet}
+          >
+            Generate GradeSheet
+          </button>
         </div>
       </>
       <div
@@ -204,55 +463,81 @@ function Result() {
               <th>Student Name</th>
               <th>Roll Number</th>
               {subjectNames.map((subject) => (
-                <th key={subject}>{subject}</th>
+                <React.Fragment key={subject}>
+                  <th>{subject}</th>
+                  {showGradePoints && <th>Grade Points</th>}
+                </React.Fragment>
               ))}
               <th>Total Obtained Marks</th>
-              <th>Rank</th> {/* New column for Rank */}
-              <th> Remarks</th>
+              <th>Total Grade Points</th>
+              <th>Rank</th>
+              <th>Remarks</th>
             </tr>
           </thead>
           <tbody>
             {obtainedMarksData.map((student) => (
-              <tr key={student.rollNumber}>
-                <td>{student.name}</td>
-                <td>{student.rollNumber}</td>
-                {subjectNames.map((subject) => {
-                  const subjectData = student.subjects.find(
-                    (s) => s.subject === subject
-                  );
-                  const isFailed =
-                    subjectData &&
-                    subjectData.obtainedMarks < subjectData.passMarks;
-                  return (
-                    <td
-                      key={subject}
-                      style={{
-                        backgroundColor: isFailed ? "#f2f2f2" : "",
-                        fontWeight: isFailed ? "bold" : "normal",
-                      }}
-                    >
-                      {subjectData && subjectData.obtainedMarks
-                        ? subjectData.obtainedMarks
-                        : ""}
-                    </td>
-                  );
-                })}
-                <td>{student.totalMarks}</td>
-                <td>{student.rank}</td>
-                <td>
-                  {student.subjects.every(
-                    (subject) => subject.obtainedMarks >= subject.passMarks
-                  )
-                    ? "Passed"
-                    : "Failed"}
-                </td>
-              </tr>
+              <React.Fragment key={student.rollNumber}>
+                <tr>
+                  <td>{student.name}</td>
+                  <td>{student.rollNumber}</td>
+                  {subjectNames.map((subject) => {
+                    const subjectData = student.subjects.find(
+                      (s) => s.subject === subject
+                    );
+                    const isFailed =
+                      subjectData &&
+                      (subjectData.obtainedMarks === undefined ||
+                        parseInt(subjectData.obtainedMarks, 10) <
+                          subjectData.passMarks);
+
+                    return (
+                      <React.Fragment key={subject}>
+                        <td
+                          style={{
+                            backgroundColor: isFailed ? "#f2f2f2" : "",
+                            fontWeight: isFailed ? "bold" : "normal",
+                          }}
+                        >
+                          {subjectData && subjectData.obtainedMarks
+                            ? subjectData.obtainedMarks
+                            : ""}
+                        </td>
+                        {showGradePoints && (
+                          <td>
+                            {subjectData
+                              ? calculateSubjectGradePoints(
+                                  subjectData.obtainedMarks,
+                                  subjectData.fullMarks
+                                ).toFixed(2)
+                              : ""}
+                          </td>
+                        )}
+                      </React.Fragment>
+                    );
+                  })}
+                  <td>{student.totalMarks}</td>
+                  <td>{student.totalGradePoints}</td>
+                  <td>{student.rank}</td>
+                  <td>
+                    {student.subjects.every(
+                      (subject) =>
+                        parseInt(subject.obtainedMarks, 10) >= subject.passMarks
+                    )
+                      ? "Passed"
+                      : "Failed"}
+                  </td>
+                </tr>
+              </React.Fragment>
             ))}
           </tbody>
         </table>
       </div>
-      <button onClick={handlePrint} disabled={pdfLoading}>
-        {pdfLoading ? "Generating PDF..." : "Print/Download Table"}
+      <button
+        className="btn btn-primary"
+        onClick={handlePrint}
+        disabled={pdfLoading}
+      >
+        {pdfLoading ? "Generating PDF..." : "Print"}
       </button>
     </div>
   );
